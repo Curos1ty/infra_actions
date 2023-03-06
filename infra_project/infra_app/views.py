8e1c5354363fe6ecc578c4f2c884991f7e48e53f
Михаил Передереев <curosity.hater@gmail.com>
2023-03-06 15:55:47 +0300

    add text in index page

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('У меня получилось!')
+    return HttpResponse('У меня получилось! Я в принципе даже и не удивлен')
 
 
 def second_page(request):
